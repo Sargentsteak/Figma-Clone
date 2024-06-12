992ad7b1b61fe2a3a071cccc08a10a14b116bc53
Sargentsteak <piyushjsb@gmail.com>
2024-06-13 00:06:05 +0530

    commit

--- my-app/components/cursor/LiveCursors.tsx
+++ my-app/components/cursor/LiveCursors.tsx
@@ -4,19 +4,23 @@ import React from "react";
 import Cursor from "./Cursor";
 
 const LiveCursors = ({ others }: LiveCursorProps) => {
-  others.map(({ connectionId, presence }) => {
-    if (!presence) return null;
+  return (
+    <>
+      {others.map(({ connectionId, presence }) => {
+        if (!presence) return null;
 
-    return (
-      <Cursor
-        key={connectionId}
-        color={COLORS[Number(connectionId % COLORS.length)]}
-        x={presence.cursor.x}
-        y={presence.cursor.y}
-        message={presence.message}
-      />
-    );
-  });
+        return (
+          <Cursor
+            key={connectionId}
+            color={COLORS[connectionId % COLORS.length]}
+            x={presence.cursor.x}
+            y={presence.cursor.y}
+            message={presence.message}
+          />
+        );
+      })}
+    </>
+  );
 };
 
 export default LiveCursors;
